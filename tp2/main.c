e7750cc7e8713fbd9029835e8fab582f1ed4012f
Christian Grimberg <christian.grimberg@outlook.com>
2019-09-07 23:13:12 -0300

    :monkey_face: Compilacion con aplicacion make a partir de un makefile.

---
+++ tp2/main.c
@@ -0,0 +1,19 @@
+/*******************************************************************
+ * Programa: Nomina de Empleados
+ *
+ * Objetivo:
+ *      Se requiere administrar el ABM de una nomina de 100 empleados.
+ *
+ * Version:
+ *      v0.1 - 7 de septiembre de 2019
+ *
+ * Autor: Christian Grimberg
+ *
+ ********************************************************************/
+#include "menu.h"
+
+int main()
+{
+    printf("Trabajo Practico 2\n");
+    return 0;
+}
